1f191d42be1edf7aa5c55933fc080c9835e7ad40
Erika <erika.paige.handley@gmail.com>
2020-05-21 18:26:14 -0700

    added gitignore. coded 'server.js'

---
+++ server.js
@@ -0,0 +1,16 @@
+//bring in express server
+const express = require('express')
+//bring in join method from path
+const { join } = require('path')
+//build new express app
+const app = express()
+
+//build server.js 'boiler plate' (declare middlewear)
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+//bringing in routes from 'routes file'
+app.use(require('./Develop/routes/listRoutes.js'))
+
+app.listen(3000, () => console.log('http://localhost:3000'))
